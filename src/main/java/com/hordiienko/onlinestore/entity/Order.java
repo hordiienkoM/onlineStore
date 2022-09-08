857643ca96574c540b194b3033a870cefd1a034c
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-08 16:00:39 +0300

    Deleted "model", getters and setters, fixed "dateCreation", rename to "createDate"

--- src/main/java/com/hordiienko/onlinestore/entity/Order.java
+++ src/main/java/com/hordiienko/onlinestore/entity/Order.java
@@ -14,10 +14,10 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String status;
-    private GregorianCalendar dateCreation;
+    private GregorianCalendar createDate;
     private String address;
     @ManyToOne()
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     @OneToMany(
@@ -26,52 +26,4 @@ public class Order {
             mappedBy = "order"
     )
     private List<OrderProduct> OrderProduct = new ArrayList<>();
-
-    public List<com.hordiienko.onlinestore.entity.OrderProduct> getOrderProduct() {
-        return OrderProduct;
-    }
-
-    public void setOrderProduct(List<com.hordiienko.onlinestore.entity.OrderProduct> orderProduct) {
-        OrderProduct = orderProduct;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public GregorianCalendar getDateCreation() {
-        return dateCreation;
-    }
-
-    public void setDateCreation(GregorianCalendar dateCreation) {
-        this.dateCreation = dateCreation;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
--- src/main/java/com/hordiienko/onlinestore/entity/OrderProduct.java
+++ src/main/java/com/hordiienko/onlinestore/entity/OrderProduct.java
@@ -22,35 +22,4 @@ public class OrderProduct {
     @JoinColumn(name="product_id", nullable=false)
     private Product product;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
 }
