cea0cba28bb9a72d9013af976fc0778ed0233bdd
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-08 14:50:57 +0300

    Added "mappedBy" to Order and Product

--- src/main/java/com/hordiienko/onlinestore/entity/Product.java
+++ src/main/java/com/hordiienko/onlinestore/entity/Product.java
@@ -2,6 +2,8 @@ package com.hordiienko.onlinestore.entity;
 
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "product")
@@ -10,6 +12,21 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            mappedBy = "product"
+    )
+    private List<OrderProduct> orderProduct = new ArrayList<>();
+
+    public List<OrderProduct> getOrderProduct() {
+        return orderProduct;
+    }
+
+    public void setOrderProduct(List<OrderProduct> orderProduct) {
+        this.orderProduct = orderProduct;
+    }
 
     public Long getId() {
         return id;
