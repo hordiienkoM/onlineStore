bb5b7b400b4fce191fd5e764f2f3154519635e80
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-08 18:14:12 +0300

    Added methods "getDate" and "getStatus" to class OrderController

---
+++ src/main/java/com/hordiienko/onlinestore/exception/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hordiienko.onlinestore.exception;
+
+public class OrderNotFoundException extends Exception{
+    public OrderNotFoundException (){
+        super("Order not found");
+    }
+}
--- src/main/java/com/hordiienko/onlinestore/repository/UserRepository.java
+++ src/main/java/com/hordiienko/onlinestore/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findUserByName(String name);
+    List<Order> findAllById(Long id);
 }
