abc4c8c48339b3ec3bebf3db65663247e30baaa9
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-09 11:07:27 +0300

    Added methods "getUser" to class UserController

--- src/main/java/com/hordiienko/onlinestore/entity/User.java
+++ src/main/java/com/hordiienko/onlinestore/entity/User.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -24,5 +24,5 @@ public class User {
             cascade = CascadeType.ALL,
             orphanRemoval = true
     )
-    private List<Order> orders = new ArrayList<>();
+    private Set<Order> orders = new HashSet<>();
 }
--- src/main/java/com/hordiienko/onlinestore/service/OrderService.java
+++ src/main/java/com/hordiienko/onlinestore/service/OrderService.java
@@ -3,27 +3,32 @@ package com.hordiienko.onlinestore.service;
 import com.hordiienko.onlinestore.entity.Order;
 import com.hordiienko.onlinestore.entity.User;
 import com.hordiienko.onlinestore.exception.OrderNotFoundException;
+import com.hordiienko.onlinestore.exception.UserNotFoundException;
 import com.hordiienko.onlinestore.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.GregorianCalendar;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private UserService userService;
 
     public Set<Order> getOrders(Long userId)throws OrderNotFoundException {
         try {
-            return orderRepository.findAllByUserId(userId);
+            User user = userService.getUser(userId);
+            return user.getOrders();
         } catch (Exception e) {
             throw  new OrderNotFoundException();
         }
     }
 
-    public GregorianCalendar getDate (Long id) throws OrderNotFoundException {
+    public GregorianCalendar getDate(Long id) throws OrderNotFoundException {
         try {
             return orderRepository.findById(id).get().getCreateDate();
         } catch (Exception e) {
@@ -31,11 +36,19 @@ public class OrderService {
         }
     }
 
-    public String getStatus (Long id) throws OrderNotFoundException {
+    public String getStatus(Long id) throws OrderNotFoundException {
         try {
             return orderRepository.findById(id).get().getStatus();
         } catch (Exception e) {
             throw  new OrderNotFoundException();
         }
+    }
+
+    public User getUser(Long orderId) throws OrderNotFoundException {
+        Optional<Order> order = orderRepository.findById(orderId);
+        if(order.isPresent()){
+            return order.get().getUser();
+        }
+        throw new OrderNotFoundException();
     }
 }
