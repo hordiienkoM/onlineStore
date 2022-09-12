074024ccfc5e906ad99de9657a0acf3f68e2d3dd
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-12 13:58:55 +0300

    update mapper (not working version)

---
+++ src/main/java/com/hordiienko/onlinestore/mapper/MapstructMapper.java
@@ -0,0 +1,26 @@
+package com.hordiienko.onlinestore.mapper;
+
+import com.hordiienko.onlinestore.dto.OrderGetDTO;
+import com.hordiienko.onlinestore.dto.OrderProductGetDTO;
+import com.hordiienko.onlinestore.dto.ProductGetDTO;
+import com.hordiienko.onlinestore.dto.UserGetDTO;
+import com.hordiienko.onlinestore.entity.Order;
+import com.hordiienko.onlinestore.entity.OrderProduct;
+import com.hordiienko.onlinestore.entity.Product;
+import com.hordiienko.onlinestore.entity.User;
+import org.mapstruct.Mapper;
+
+import java.util.Set;
+
+@Mapper (
+        componentModel = "spring"
+)
+public interface MapstructMapper {
+    OrderGetDTO orderToOrderGetDTO(Order order);
+    UserGetDTO userToUserGetDTO(User user);
+    OrderProductGetDTO toOrderProductGetDTO(OrderProduct orderProduct);
+    ProductGetDTO toOrderProductGetDTO(Product product);
+    Set<OrderGetDTO> toOrderGetDTOs(Set<Order> orders);
+    Set<UserGetDTO> toUserGetDTOs(Set<User> users);
+}
+
