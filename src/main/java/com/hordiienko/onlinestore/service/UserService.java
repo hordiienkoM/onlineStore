da13e0382049a1bfee7798a38820d614145f30bc
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-15 11:05:06 +0300

    renamed API links

--- src/main/java/com/hordiienko/onlinestore/service/UserService.java
+++ src/main/java/com/hordiienko/onlinestore/service/UserService.java
@@ -1,5 +1,6 @@
 package com.hordiienko.onlinestore.service;
 
+import com.hordiienko.onlinestore.entity.Order;
 import com.hordiienko.onlinestore.entity.User;
 import com.hordiienko.onlinestore.exception.UserNotFoundException;
 import com.hordiienko.onlinestore.repository.UserRepository;
@@ -7,17 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User getUser(Long userId) throws UserNotFoundException {
-        Optional<User> user = userRepository.findById(userId);
-        if (user.isPresent()) {
-            return user.get();
-        }
-        throw new UserNotFoundException();
+    public User getUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow();
+    }
+
+    public Set<Order> getOrders(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        return user.getOrders();
     }
 }
