c425962da0e81134556f18d907acf4346c4b82cf
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-16 16:10:38 +0300

    Changed @RestController to @Controller, implemented output list of products to web page

--- src/main/java/com/hordiienko/onlinestore/controller/ProductController.java
+++ src/main/java/com/hordiienko/onlinestore/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.hordiienko.onlinestore.controller;
 
 
+import com.hordiienko.onlinestore.dto.ProductGetDTO;
 import com.hordiienko.onlinestore.entity.Product;
 import com.hordiienko.onlinestore.mapper.ProductMapper;
 import com.hordiienko.onlinestore.service.ProductService;
@@ -8,10 +9,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @AllArgsConstructor
 @RequestMapping("/v1/products")
 public class ProductController {
@@ -21,10 +23,21 @@ public class ProductController {
     private ProductMapper productMapper;
 
     @GetMapping
-    public ResponseEntity getProductsPage(@RequestParam Integer page,
-                                           @RequestParam Integer pageSize,
-                                           @RequestParam String sortField){
-        Page<Product> pageProductsDTO = productService.getProducts(page, pageSize, sortField);
-        return ResponseEntity.ok().body(productMapper.toProductGetDTOs(pageProductsDTO));
+    public String getProductsPage(@RequestParam Integer page,
+                                          @RequestParam Integer pageSize,
+                                          @RequestParam String sortField,
+                                                        Model model){
+        Page<Product> pageProducts = productService.getProducts(page, pageSize, sortField);
+        Page<ProductGetDTO> pageProductsDTO = productMapper.toProductGetDTOs(pageProducts);
+        model.addAttribute("products", pageProductsDTO);
+        return "products_page";
     }
+
+//    @GetMapping
+//    public ResponseEntity getProductsPage(@RequestParam Integer page,
+//                                           @RequestParam Integer pageSize,
+//                                           @RequestParam String sortField){
+//        Page<Product> pageProductsDTO = productService.getProducts(page, pageSize, sortField);
+//        return ResponseEntity.ok().body(productMapper.toProductGetDTOs(pageProductsDTO));
+//    }
 }
