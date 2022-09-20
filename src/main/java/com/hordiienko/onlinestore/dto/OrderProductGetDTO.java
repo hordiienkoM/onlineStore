379a0ac8ab0214c8f43d9b88593865b59172d32e
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-20 16:57:59 +0300

    added scrolling orders

--- src/main/java/com/hordiienko/onlinestore/dto/OrderProductGetDTO.java
+++ src/main/java/com/hordiienko/onlinestore/dto/OrderProductGetDTO.java
@@ -1,12 +1,14 @@
 package com.hordiienko.onlinestore.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderProductGetDTO {
+    @JsonProperty("products")
     private ProductGetDTO product;
     private Integer amount;
 }
