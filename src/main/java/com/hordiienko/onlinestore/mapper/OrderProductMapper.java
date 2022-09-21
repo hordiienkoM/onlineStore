35b03cd83ef259912e4497ac3d5ca87c44fe3e1e
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-21 12:19:48 +0300

    added "get OrderProduct by order id" api

--- src/main/java/com/hordiienko/onlinestore/mapper/OrderProductMapper.java
+++ src/main/java/com/hordiienko/onlinestore/mapper/OrderProductMapper.java
@@ -5,9 +5,13 @@ import com.hordiienko.onlinestore.dto.OrderProductPostDTO;
 import com.hordiienko.onlinestore.entity.OrderProduct;
 import org.mapstruct.Mapper;
 
+import java.util.Set;
+
 @Mapper
 public interface OrderProductMapper {
     OrderProductGetDTO toOrderProductGetDTO(OrderProduct orderProduct);
 
     OrderProduct toOrderProduct(OrderProductPostDTO orderProductPostDTO);
+
+    Set<OrderProductGetDTO> toOrderProductGetDTOs(Set<OrderProduct> orderProducts);
 }
