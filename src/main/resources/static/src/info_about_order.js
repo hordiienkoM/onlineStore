4a21245c5248411c379b47bb145987d36b1468f1
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-22 15:56:32 +0300

    added view-status update after deleting

--- src/main/resources/static/src/info_about_order.js
+++ src/main/resources/static/src/info_about_order.js
@@ -41,22 +41,3 @@ function writeProductsTable() {
         }
     });
 }
-
-function deleteOrderById(orderId){
-    $.ajax({
-        url: "http://localhost:8080/v1/orders/" + orderId,
-        method: "DELETE",
-        success: function (data) {
-            if (data)
-            {
-                alert("Order " + orderId + " was deleted")
-
-            }
-            else
-            {
-                alert("Order not found");
-            }
-        }
-    });
-    $("#order_view").hide();
-}
