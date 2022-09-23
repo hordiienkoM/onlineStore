1c439a342f55b4fb695541aad8588cea7ba45bd3
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-23 17:21:35 +0300

    added simple authorization

---
+++ src/main/java/com/hordiienko/onlinestore/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.hordiienko.onlinestore;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/online_shop").setViewName("online_shop");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
