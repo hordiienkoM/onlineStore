562042d636d6fe146ebee47380de87c5dbd93345
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-26 12:02:08 +0300

    updated pom.xml

--- src/main/java/com/hordiienko/onlinestore/entity/User.java
+++ src/main/java/com/hordiienko/onlinestore/entity/User.java
@@ -3,8 +3,11 @@ package com.hordiienko.onlinestore.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,12 +20,13 @@ import java.util.Set;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
+    private String role;
+
     @JsonIgnore
     private String password;
     @OneToMany(
@@ -31,4 +35,34 @@ public class User {
             mappedBy = "user"
     )
     private Set<Order> orders = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
