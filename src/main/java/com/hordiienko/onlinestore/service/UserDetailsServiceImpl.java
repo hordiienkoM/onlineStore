67ea2ac80432949f280a6320273975956650776a
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-29 17:25:01 +0300

    "User extends UserDetails" --> "User"&&"UserDetailsImpl"

---
+++ src/main/java/com/hordiienko/onlinestore/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.hordiienko.onlinestore.service;
+
+
+import com.hordiienko.onlinestore.dto.authorization.UserDetailsImpl;
+import com.hordiienko.onlinestore.entity.User;
+import com.hordiienko.onlinestore.exception.UserNotFoundException;
+import com.hordiienko.onlinestore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserDetailsImpl(user);
+    }
+}
