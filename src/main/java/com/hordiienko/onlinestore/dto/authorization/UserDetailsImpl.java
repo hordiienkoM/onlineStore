685ce20b329f198e1b37dc17f41d763718b8b743
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-29 17:25:01 +0300

    "User extends UserDetails" --> "User"&&"UserDetailsImpl"

---
+++ src/main/java/com/hordiienko/onlinestore/dto/authorization/UserDetailsImpl.java
@@ -0,0 +1,59 @@
+package com.hordiienko.onlinestore.dto.authorization;
+
+
+import com.hordiienko.onlinestore.entity.Role;
+import com.hordiienko.onlinestore.entity.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Set<Role> getAuthorities() {
+        return user.getRoles();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
