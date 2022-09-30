83bd2f8b6b8f0968fa737fd3f9bbe5e182d7edf6
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-30 11:15:47 +0300

    moved logic "UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();" from Controller to Service

--- src/main/java/com/hordiienko/onlinestore/service/OrderService.java
+++ src/main/java/com/hordiienko/onlinestore/service/OrderService.java
@@ -2,6 +2,7 @@ package com.hordiienko.onlinestore.service;
 
 import com.hordiienko.onlinestore.dto.OrderPostDTO;
 import com.hordiienko.onlinestore.dto.OrderProductPostDTO;
+import com.hordiienko.onlinestore.dto.authorization.UserDetailsImpl;
 import com.hordiienko.onlinestore.entity.Order;
 import com.hordiienko.onlinestore.entity.OrderProduct;
 import com.hordiienko.onlinestore.exception.OrderNotFoundException;
@@ -10,13 +11,12 @@ import com.hordiienko.onlinestore.exception.UserNotFoundException;
 import com.hordiienko.onlinestore.repository.OrderProductRepository;
 import com.hordiienko.onlinestore.repository.OrderRepository;
 import com.hordiienko.onlinestore.repository.UserRepository;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.security.Principal;
 import java.util.Set;
 
 @Service
@@ -30,13 +30,12 @@ public class OrderService {
     @Autowired
     private OrderProductRepository orderProductRepository;
     @Autowired
-    private SessionService sessionService;
-    @Autowired
     private UserRepository userRepository;
 
-    public Order saveOrder(Order order, Set<OrderProductPostDTO> products, Long userId) throws OrderSaveException {
+    public Order saveOrder(Order order, Set<OrderProductPostDTO> products, Authentication authentication) throws OrderSaveException {
         try {
-            order.setUser(userRepository.findById(userId).orElseThrow());
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+            order.setUser(userRepository.findById(userDetails.getUserId()).orElseThrow());
             Set<OrderProduct> orderProducts = orderProductService.convert(products, order);
             order.setOrderProduct(orderProducts);
             orderRepository.save(order);
@@ -46,10 +45,11 @@ public class OrderService {
         }
     }
 
-    public void deleteOrder(Long orderId, Long currentUserId) throws OrderNotFoundException {
+    public void deleteOrder(Long orderId, Authentication authentication) throws OrderNotFoundException {
         try {
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
             Order order = orderRepository.findById(orderId).orElseThrow();
-            if (checkUserHasOrder(order, currentUserId)) {
+            if (checkUserHasOrder(order, userDetails.getUserId())) {
                 orderRepository.deleteById(orderId);
             } else {
                 throw new Exception();
@@ -69,9 +69,10 @@ public class OrderService {
         orderRepository.save(order);
     }
 
-    public Order getOrder(Long orderId, Long currentUserId) throws Exception {
+    public Order getOrder(Long orderId, Authentication authentication) throws Exception {
         Order order = orderRepository.findById(orderId).orElseThrow();
-        if(checkUserHasOrder(order, currentUserId)){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        if(checkUserHasOrder(order, userDetails.getUserId())){
             return order;
         } else {
             throw new Exception("Order not found");
@@ -83,11 +84,12 @@ public class OrderService {
         return orderUserId.equals(currentUserId);
     }
 
-    public Page<Order> getByUserId(Pageable pageable, Long userId){
-        return orderRepository.findAllByUserId(userId, pageable);
+    public Page<Order> getByUserId(Pageable pageable, Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return orderRepository.findAllByUserId(userDetails.getUserId(), pageable);
     }
 
-    public Set<OrderProduct> getProductsByOrderId(Long orderId, Long currentUserId) throws Exception {
-        return getOrder(orderId, currentUserId).getOrderProduct();
+    public Set<OrderProduct> getProductsByOrderId(Long orderId, Authentication authentication) throws Exception {
+        return getOrder(orderId, authentication).getOrderProduct();
     }
 }
