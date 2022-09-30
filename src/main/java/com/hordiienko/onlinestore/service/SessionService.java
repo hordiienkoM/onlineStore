4a99529e3ff36712d5128f2139df2a9232996641
developer <mikhail.gordiyenko@faceit-team.com>
2022-09-30 12:03:55 +0300

    create SessionUtil with method SessionUtil.getCurrentUserId(Authentication authentication), changed ((UserDetailsService)authentication.getPrincipal()).getUserId to this method.

--- src/main/java/com/hordiienko/onlinestore/service/SessionService.java
+++ src/main/java/com/hordiienko/onlinestore/service/SessionService.java
@@ -1,15 +1,11 @@
 package com.hordiienko.onlinestore.service;
 
-import com.hordiienko.onlinestore.entity.User;
-import com.hordiienko.onlinestore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SessionService {
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private UserService userService;
@@ -17,11 +13,5 @@ public class SessionService {
         String username = SecurityContextHolder
                 .getContext().getAuthentication().getName();
         return userService.findByUsername(username).getId();
-    }
-
-    public User getCurrenUser(){
-        String username = SecurityContextHolder
-                .getContext().getAuthentication().getName();
-        return userRepository.findByUsername(username);
     }
 }
